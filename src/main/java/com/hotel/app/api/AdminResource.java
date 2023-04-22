d9f0eda0086933bf45e770f37b6e487b43adba5e
“Luisa <xtremelu@gmail.com>
2023-04-22 14:26:32 +0100

    :: Add javadoc to class methods in AdminResource.java

--- src/main/java/com/hotel/app/api/AdminResource.java
+++ src/main/java/com/hotel/app/api/AdminResource.java
@@ -21,9 +21,14 @@ public class AdminResource {
 
     private static AdminResource instance;
 
-    private AdminResource() {
-    }
+    // This constructor his hidden because this class is a Singleton.
+    private AdminResource() {}
 
+    /**
+     * Get the AdminResource instance.
+     *
+     * @return the AdminResource singleton object.
+     */
     public static AdminResource getInstance() {
         if (instance == null) {
             instance = new AdminResource();
@@ -31,22 +36,46 @@ public class AdminResource {
         return instance;
     }
 
+    /**
+     * Get customer with the specified email.
+     *
+     * @param email - the specified email.
+     * @return the Customer with the given email, or null if such Customer does not exist.
+     */
     public Customer getCustomer(String email) {
         return CustomerService.getInstance().getCustomer(email);
     }
 
+    /**
+     * Add the given IRoom object which represents a room.
+     *
+     * @param room - the given room.
+     */
     public void addRoom(IRoom room) {
         ReservationService.getInstance().addIRoom(room);
     }
 
+    /**
+     * Get all rooms.
+     *
+     * @return a collection of IRoom instances containing all rooms.
+     */
     public Collection<IRoom> getAllRooms() {
         return ReservationService.getInstance().getAllRooms();
     }
 
+    /**
+     * Get all customers.
+     *
+     * @return a collection of Customer instances containing all customers.
+     */
     public Collection<Customer> getAllCustomers(){
         return CustomerService.getInstance().getAllCustomers();
     }
 
+    /**
+     * Prints all reservations.
+     */
     public void displayAllReservations() {
         ReservationService.getInstance().printAllReservations();
     }
