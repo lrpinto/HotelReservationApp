89700a13703da2bdd13e2835f4e3fc52836e23b1
“Luisa <xtremelu@gmail.com>
2023-04-22 22:53:37 +0100

    :: Add javadoc to CustomerService.java

--- src/main/java/com/hotel/app/service/CustomerService.java
+++ src/main/java/com/hotel/app/service/CustomerService.java
@@ -5,16 +5,31 @@ import com.hotel.app.model.Customer;
 import java.util.Collection;
 import java.util.HashMap;
 
+/**
+ * CustomerService.java
+ *
+ * A class which provides Customer services.
+ *
+ * @author Luisa Pinto | luisa.rebelopinto@gmail.com
+ */
 public class CustomerService {
 
+    // The singleton instance of CustomerService
     private static CustomerService instance;
 
+    // A hash map to store customers indexed by email address.
     private final HashMap<String, Customer> customers;
 
+    // Hides the constructor since this class is a singleton
     private CustomerService() {
         this.customers = new HashMap<>();
     }
 
+    /**
+     * Get the singleton CustomerService instance.
+     *
+     * @return the CustomerService instance.
+     */
     public static CustomerService getInstance() {
         if (instance == null) {
             instance = new CustomerService();
@@ -22,6 +37,13 @@ public class CustomerService {
         return instance;
     }
 
+    /**
+     * Add the customer with the given email, first name, and last name.
+     *
+     * @param email - the given email
+     * @param firstName - the given first name
+     * @param lastName - the given last name
+     */
     public void addCustomer(String email, String firstName, String lastName) {
         if (this.customers.containsKey(email)) {
             throw new IllegalArgumentException("A customer with this email already exists");
@@ -31,10 +53,21 @@ public class CustomerService {
         this.customers.put(email, customer);
     }
 
+    /**
+     * Get the Customer instance with the given email address.
+     *
+     * @param email - the given email address.
+     * @return a Customer instance with the given email, or null if it does not exists.
+     */
     public Customer getCustomer(String email) {
         return this.customers.get(email);
     }
 
+    /**
+     * Get the all Customer instances held by this CustomerService instance.
+     *
+     * @return a collection of Customer instances.
+     */
     public Collection<Customer> getAllCustomers() {
         return this.customers.values();
     }
