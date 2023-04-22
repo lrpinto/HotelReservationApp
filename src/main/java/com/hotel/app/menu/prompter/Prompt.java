b2c7dafc2fa0e09743d2f1127ea3ac7506b9db01
“Luisa <xtremelu@gmail.com>
2023-04-22 22:12:03 +0100

    :: Add class javadoc to Prompt.java

--- src/main/java/com/hotel/app/menu/prompter/Prompt.java
+++ src/main/java/com/hotel/app/menu/prompter/Prompt.java
@@ -1,6 +1,14 @@
 package com.hotel.app.menu.prompter;
 
+/**
+ * Prompt.java
+ * <p>
+ * A class of constants used in user prompts by the Prompter.
+ *
+ * @author Luisa Pinto | luisa.rebelopinto@gmail.com
+ */
 public class Prompt {
+
     /**
      * The message shown to the user when requesting them to press the 'enter' key.
      */
