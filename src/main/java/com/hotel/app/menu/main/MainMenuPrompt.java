1d0e6bd6c2a37a55b11f56497b5d09a2ccdd9469
“Luisa <xtremelu@gmail.com>
2023-04-22 22:10:19 +0100

    :: Re-format file MainMenuPrompt.java

--- src/main/java/com/hotel/app/menu/main/MainMenuPrompt.java
+++ src/main/java/com/hotel/app/menu/main/MainMenuPrompt.java
@@ -10,49 +10,14 @@ package com.hotel.app.menu.main;
 public class MainMenuPrompt {
 
     /**
-     * The initial message to prompt a user for a check-in date.
-     */
-    public static final String TYPE_THE_CHECK_IN_DATE = "Please type the Check-In date, e.g. 2024-04-01: ";
-
-    /**
-     * The message to prompt a user to type a valid date matching the format 'yyyy-mm-dd'.
-     */
-    public static final String TYPE_A_VALID_DATE_USING_THE_FORMAT_YYYY_MM_DD = "Please type a valid date using the format 'yyyy-mm-dd': ";
-
-    /**
-     * The initial message to prompt a user for a check-out date.
-     */
-    public static final String TYPE_THE_CHECK_OUT_DATE = "Please type the Check-Out date, e.g. 2024-04-10: ";
-
-    /**
-     * The initial message to prompt a user whether they would like to create another customer.
-     */
-    public static final String WOULD_YOU_LIKE_TO_ADD_ANOTHER_CUSTOMER = "Would you like to add another customer - answer 'y' or 'n': ";
-
-    /**
-     * The initial message to prompt a user for their email address.
-     */
-    public static final String TYPE_YOUR_EMAIL_AND_PRESS_ENTER = "Please type your email and press enter: ";
-
-    /**
      * The message to prompt a user the email address after they inserted an invalid email address.
      */
     public static final String ENTER_A_VALID_EMAIL_ADDRESS = "Please enter a valid email address: ";
 
     /**
-     * The initial message to prompt a user for their first name.
-     */
-    public static final String TYPE_YOUR_FIRST_NAME_AND_PRESS_ENTER = "Please type your first name and press enter: ";
-
-    /**
      * The message to prompt a user for a first name after they inserted an invalid name.
      */
     public static final String ENTER_A_VALID_FIRST_NAME = "Please enter a valid first name: ";
-
-    /**
-     * The initial message to prompt a user for their last name.
-     */
-    public static final String TYPE_YOUR_LAST_NAME_AND_PRESS_ENTER = "Please type your last name and press enter: ";
 
     /**
      * The message to prompt a user for a last name after they inserted an invalid name.
@@ -65,9 +30,24 @@ public class MainMenuPrompt {
     public static final String ENTER_Y_YES_OR_N_NO = "Please enter y (yes) or n (no): ";
 
     /**
-     * The message to prompt a user whether they would like to reserve a room.
+     * The message to prompt a user to type a valid date matching the format 'yyyy-mm-dd'.
      */
-    public static final String WOULD_YOU_LIKE_TO_RESERVE_A_ROOM = "Would you like to reserve a room - answer 'y' or 'n': ";
+    public static final String TYPE_A_VALID_DATE_USING_THE_FORMAT_YYYY_MM_DD = "Please type a valid date using the format 'yyyy-mm-dd': ";
+
+    /**
+     * The message to prompt a user for a last name after they inserted an invalid room number.
+     */
+    public static final String TYPE_A_VALID_ROOM_NUMBER = "Please type a valid room number: ";
+
+    /**
+     * The initial message to prompt a user for a check-in date.
+     */
+    public static final String TYPE_THE_CHECK_IN_DATE = "Please type the Check-In date, e.g. 2024-04-01: ";
+
+    /**
+     * The initial message to prompt a user for a check-out date.
+     */
+    public static final String TYPE_THE_CHECK_OUT_DATE = "Please type the Check-Out date, e.g. 2024-04-10: ";
 
     /**
      * The initial message to prompt a user for a room number.
@@ -75,8 +55,28 @@ public class MainMenuPrompt {
     public static final String TYPE_THE_ROOM_NUMBER_AND_PRESS_ENTER = "Please type the room number and press enter: ";
 
     /**
-     * The message to prompt a user for a last name after they inserted an invalid room number.
+     * The initial message to prompt a user for their email address.
      */
-    public static final String TYPE_A_VALID_ROOM_NUMBER = "Please type a valid room number: ";
+    public static final String TYPE_YOUR_EMAIL_AND_PRESS_ENTER = "Please type your email and press enter: ";
+
+    /**
+     * The initial message to prompt a user for their first name.
+     */
+    public static final String TYPE_YOUR_FIRST_NAME_AND_PRESS_ENTER = "Please type your first name and press enter: ";
+
+    /**
+     * The initial message to prompt a user for their last name.
+     */
+    public static final String TYPE_YOUR_LAST_NAME_AND_PRESS_ENTER = "Please type your last name and press enter: ";
+
+    /**
+     * The initial message to prompt a user whether they would like to create another customer.
+     */
+    public static final String WOULD_YOU_LIKE_TO_ADD_ANOTHER_CUSTOMER = "Would you like to add another customer - answer 'y' or 'n': ";
+
+    /**
+     * The message to prompt a user whether they would like to reserve a room.
+     */
+    public static final String WOULD_YOU_LIKE_TO_RESERVE_A_ROOM = "Would you like to reserve a room - answer 'y' or 'n': ";
 
 }
