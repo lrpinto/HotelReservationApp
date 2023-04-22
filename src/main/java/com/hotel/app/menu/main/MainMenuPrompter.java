24cc6934d3d6ca7b535f62614805a6adaf6e94ea
“Luisa <xtremelu@gmail.com>
2023-04-22 21:37:15 +0100

    :: Add javadoc to public strings in MainMenuPrompter

--- src/main/java/com/hotel/app/menu/main/MainMenuPrompter.java
+++ src/main/java/com/hotel/app/menu/main/MainMenuPrompter.java
@@ -16,30 +16,80 @@ import static com.hotel.app.constant.Regex.*;
  */
 public record MainMenuPrompter(Prompter prompter) {
 
-    private static final String CHECK_IN_DATE_PRIMARY_PROMPT = "Please type the Check-In date, e.g. 2024-04-01: ";
-    private static final String CHECK_IN_DATE_SECONDARY_PROMPT = "Please type a valid date using the format 'yyyy-mm-dd': ";
+    /**
+     * The initial message to prompt a user for a check-in date.
+     */
+    public static final String CHECK_IN_DATE_PRIMARY_PROMPT = "Please type the Check-In date, e.g. 2024-04-01: ";
 
-    private static final String CHECK_OUT_DATE_PRIMARY_PROMPT = "Please type the Check-Out date, e.g. 2024-04-10: ";
-    private static final String CHECK_OUT_DATE_SECONDARY_PROMPT = "Please type a valid date using the format 'yyyy-mm-dd': ";
+    /**
+     * The message to prompt a user for a check-in date after they inserted an invalid date.
+     */
+    public static final String CHECK_IN_DATE_SECONDARY_PROMPT = "Please type a valid date using the format 'yyyy-mm-dd': ";
 
-    public static final String CREATE_CUSTOMER_PRIMARY_PROMPT = "Would you like to add another customer - answer 'y' or 'n': ";
+    /**
+     * The initial message to prompt a user for a check-out date.
+     */
+    public static final String CHECK_OUT_DATE_PRIMARY_PROMPT = "Please type the Check-Out date, e.g. 2024-04-10: ";
 
+    /**
+     * The message to prompt a user for a check-out date after they inserted an invalid date.
+     */
+    public static final String CHECK_OUT_DATE_SECONDARY_PROMPT = "Please type a valid date using the format 'yyyy-mm-dd': ";
 
+    /**
+     * The initial message to prompt a user whether they would like to create another customer.
+     */
+    public static final String CREATE_ANOTHER_CUSTOMER_PRIMARY_PROMPT = "Would you like to add another customer - answer 'y' or 'n': ";
+
+    /**
+     * The initial message to prompt a user for their email address.
+     */
     public static final String EMAIL_PRIMARY_PROMPT = "Please type your email and press enter: ";
+
+    /**
+     * The message to prompt a user the email address after they inserted an invalid email address.
+     */
     public static final String EMAIL_SECONDARY_PROMPT = "Please enter a valid email address: ";
 
+    /**
+     * The initial message to prompt a user for their first name.
+     */
     public static final String FIRST_NAME_PRIMARY_PROMPT = "Please type your first name and press enter: ";
+
+    /**
+     * The message to prompt a user for a first name after they inserted an invalid name.
+     */
     public static final String FIRST_NAME_SECONDARY_PROMPT = "Please enter a valid first name: ";
 
+    /**
+     * The initial message to prompt a user for their last name.
+     */
     public static final String LAST_NAME_PRIMARY_PROMPT = "Please type your last name and press enter: ";
+
+    /**
+     * The message to prompt a user for a last name after they inserted an invalid name.
+     */
     public static final String LAST_NAME_SECONDARY_PROMPT = "Please enter a valid last name: ";
 
+    /**
+     * The message to prompt a user for a yes or no answer.
+     */
     public static final String YES_OR_NO_SECONDARY_PROMPT = "Please enter y (yes) or n (no): ";
 
-    private static final String RESERVE_ROOM_PRIMARY_PROMPT = "Would you like to reserve a room - answer 'y' or 'n': ";
+    /**
+     * The message to prompt a user whether they would like to reserve a room.
+     */
+    public static final String RESERVE_ROOM_PRIMARY_PROMPT = "Would you like to reserve a room - answer 'y' or 'n': ";
 
-    private static final String ROOM_NUMBER_PRIMARY_PROMPT = "Please type the room number and press enter: ";
-    private static final String ROOM_NUMBER_SECONDARY_PROMPT = "Please type a valid room number: ";
+    /**
+     * The initial message to prompt a user for room number.
+     */
+    public static final String ROOM_NUMBER_PRIMARY_PROMPT = "Please type the room number and press enter: ";
+
+    /**
+     * The message to prompt a user for a last name after they inserted an invalid room number.
+     */
+    public static final String ROOM_NUMBER_SECONDARY_PROMPT = "Please type a valid room number: ";
 
     /**
      * Prompt a user for the check-in date.
@@ -65,7 +115,7 @@ public record MainMenuPrompter(Prompter prompter) {
      * @return a String which can be y (yes) or n (no).
      */
     public String promptCreateAnotherCustomer() {
-        return prompter().promptString(CREATE_CUSTOMER_PRIMARY_PROMPT, YES_OR_NO_SECONDARY_PROMPT, YES_OR_NO_REGEX);
+        return prompter().promptString(CREATE_ANOTHER_CUSTOMER_PRIMARY_PROMPT, YES_OR_NO_SECONDARY_PROMPT, YES_OR_NO_REGEX);
     }
 
     /**
