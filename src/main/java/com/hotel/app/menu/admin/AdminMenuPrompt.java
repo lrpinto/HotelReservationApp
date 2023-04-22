18ec67f05f19535582ce49438fd28cfcd9ec8cae
“Luisa <xtremelu@gmail.com>
2023-04-22 22:03:19 +0100

    :: Add some blank lines to MainMenuPrompt.java and AdminMenuPrompt.java

--- src/main/java/com/hotel/app/menu/admin/AdminMenuPrompt.java
+++ src/main/java/com/hotel/app/menu/admin/AdminMenuPrompt.java
@@ -8,38 +8,47 @@ package com.hotel.app.menu.admin;
  * @author Luisa Pinto | luisa.rebelopinto@gmail.com
  */
 public class AdminMenuPrompt {
+
     /**
      * The message displayed to prompt a user whether they would like to add another room.
      */
     public static final String ADD_ANOTHER_ROOM_ANSWER_Y_OR_N = "Would you like to add another room - answer 'y' or 'n': ";
+
     /**
      * The message displayed to prompt a user to enter a room number.
      */
     public static final String ENTER_A_ROOM_NUMBER = "Please enter a room number, e.g. 1A: ";
+
     /**
      * The message displayed to prompt a user to enter a valid room number.
      */
     public static final String ENTER_A_VALID_ROOM_TYPE = "Please enter a valid room type - 1 for single room, or 2 for double room: ";
+
     /**
      * The message displayed to prompt a user to enter a price per night.
      */
     public static final String ENTER_PRICE_PER_NIGHT = "Enter price per night: ";
+
     /**
      * The message displayed to prompt a user to enter a room type.
      */
     public static final String ENTER_ROOM_TYPE = "Enter room type - 1 single, or 2 double: ";
+
     /**
      * The message displayed to prompt a user to enter a yes or no.
      */
     public static final String ENTER_Y_YES_OR_N_NO = "Please enter y (yes) or n (no): ";
+
     /**
      * The max value for a room type.
      */
     public static final int MAX_ROOM_TYPE = 2;
+
     /**
      * The minimum value for a room type.
      */
     public static final int MIN_ROOM_TYPE = 1;
+
     /**
      * The message displayed to prompt the user to enter a price equal or greater than zero.
      */
